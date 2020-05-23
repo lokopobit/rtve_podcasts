b34fd4c7e03aaa22770ef114072e7254b9dc9338
Manuel Rafael Vazquez Gandullo <manuelrv@ucm.es>
2020-05-23 13:20:23 +0200

    Add: compressing functionalities.
    Functions for compressing and decompressing available.

--- compress_mp3.py
+++ compress_mp3.py
@@ -13,7 +13,25 @@ Created on Fri May 22 19:36:48 2020
 import os
 
 def compress_mp3(already_compressed, data_path, mp3_names, remove_mp3 = False):
-    command0 = 'cmd /k TO& cd Desktop/rtve_podcasts '
+    """
+    Parameters
+    ----------
+    already_compressed : TYPE
+        DESCRIPTION.
+    data_path : TYPE
+        DESCRIPTION.
+    mp3_names : TYPE
+        DESCRIPTION.
+    remove_mp3 : TYPE, optional
+        DESCRIPTION. The default is False.
+
+    Returns
+    -------
+    None.
+
+    """
+    
+    command0 = 'cmd /k TO& cd C:/Users/juan/Desktop/rtve_podcasts '
     command1 = '' 
     for mp3_name in mp3_names: 
         if mp3_name.replace('mp3','pcf') in already_compressed:
@@ -22,12 +40,54 @@ def compress_mp3(already_compressed, data_path, mp3_names, remove_mp3 = False):
         command1 += ' && precomp ' + full_name
     command2 = ' && exit TO&'
     command = command0 + command1 + command2
-    os.system(command.replace('TO&', '"'))
+    error = os.system(command.replace('TO&', '"'))
     
-    if remove_mp3:
-        for mp3_name in mp3_names: os.delete(os.path.join(data_path, mp3_name))
+    if remove_mp3 and error == 0:
+        for mp3_name in mp3_names: os.remove(os.path.join(data_path, mp3_name))
+
+        
+def decompress_mp3(already_compressed, data_path, mp3_names, remove_pcf = False):
+    """
+    Parameters
+    ----------
+    already_compressed : TYPE
+        DESCRIPTION.
+    data_path : TYPE
+        DESCRIPTION.
+    mp3_names : TYPE
+        DESCRIPTION.
+    remove_pcf : TYPE, optional
+        DESCRIPTION. The default is False.
+
+    Returns
+    -------
+    None.
+
+    """
+    
+    command0 = 'cmd /k TO& cd C:/Users/juan/Desktop/rtve_podcasts '
+    command1 = '' 
+    for comp in already_compressed: 
+        if comp.replace('pcf','mp3') in mp3_names:
+            continue
+        full_name = os.path.join(data_path, comp)
+        command1 += ' && precomp -r -o' + full_name.replace('pcf','mp3') +' '+ full_name
+    command2 = ' && exit TO&'
+    command = command0 + command1 + command2
+    error = os.system(command.replace('TO&', '"'))
+    
+    if remove_pcf and error == 0:
+        for comp in already_compressed: os.remove(os.path.join(data_path, comp))
     
     
-data_path = ''
+
+# Compress and remove 
+data_path = r'C:\Users\juan\Desktop\rtve_podcasts\data\discopolis'
 already_compressed = [f for f in os.listdir(data_path) if f[-3:] == 'pcf']
-mp3_names = ['2012dic03.mp3', '2012dic04.mp3']
+mp3_names = [f for f in os.listdir(data_path) if f[-3:] == 'mp3']
+compress_mp3(already_compressed, data_path, mp3_names[:30], remove_mp3 = True)
+#decompress_mp3(already_compressed, data_path, mp3_names, remove_pcf = True)
+
+
+
+
